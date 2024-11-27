318503df616d7b22215c1678f338ecb6a72bd01c
mwdziak <mwdziak@gmail.com>
2024-11-27 10:43:25 +0100

    Part 3 of lab 6 finished

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CustomerViewSet, OrderViewSet, ProductViewSet
+from .views import (
+    CustomerViewSet,
+    OrderViewSet,
+    ProductCreateView,
+    ProductDetailView,
+    ProductListView,
+    ProductViewSet,
+)
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
@@ -9,5 +16,8 @@ router.register(r"customers", CustomerViewSet, basename="customer")
 router.register(r"orders", OrderViewSet, basename="order")
 
 urlpatterns = [
+    path("user/products/", ProductListView.as_view(), name="product_list"),
+    path("user/products/<int:pk>/", ProductDetailView.as_view(), name="product_detail"),
+    path("user/products/new/", ProductCreateView.as_view(), name="product_create"),
     path("api/", include(router.urls)),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -17,3 +17,31 @@ class CustomerViewSet(viewsets.ModelViewSet):
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+
+
+from django.views.generic import CreateView, DetailView, ListView
+
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = "product_list.html"
+    context_object_name = "products"
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = "product_create.html"
+    success_url = "../../products/"
+
+    def form_valid(self, form):
+        return super().form_valid(form)
