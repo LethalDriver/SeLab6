2d759d7319e42ac4446d7896ccfbc7dadc005825
mwdziak <mwdziak@gmail.com>
2025-01-22 10:13:16 +0100

    Introduced bugs

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.core.exceptions import ValidationError
+from django.db import models
 from django.utils import timezone
 
 # Create your models here.
@@ -12,9 +12,7 @@ def validate_price(value):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(
-        max_digits=10, decimal_places=2, validators=[validate_price]
-    )
+    price = models.DecimalField(max_digits=3, decimal_places=2, validators=[])
     available = models.BooleanField()
 
 
